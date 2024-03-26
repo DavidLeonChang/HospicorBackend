3aeb07593ed660ea8b479b80d3c56e995a0904e6
DavidLeonChang <dakileon@espol.edu.ec>
2024-03-26 17:15:01 -0500

    Rutas de Nutricion y controlladores

--- src/config/configuracion.js
+++ src/config/configuracion.js
@@ -48,60 +48,122 @@ const config = {
 
 
 //---------------------------------------------- Mensajes de Pacientes Inicio -----------------------------------------
-        s: {
+        tipoidentidad: {
+            createS: 'El tipo de identificación ha sido registrado',
+            createF: 'El tipo de identificacion no pudo ser creado',
+            deleteS: 'El tipo de identificación ha sido eliminado',
+            deleteF: 'Error al eliminar tipo de identidad'             
+        },
+        estadocivil: {
+            createS: 'El estado civil ha sido registrado',
+            createF: 'El estado civil no pudo ser creado',
+            deleteS: 'El estado civil ha sido eliminado',
+            deleteF: 'Error al eliminar estado civil'             
+        },
+        etnia: {
+            createS: 'La etnia ha sido registrada',
+            createF: 'La etnia no pudo ser creada',
+            deleteS: 'La etnia ha sido eliminada',
+            deleteF: 'Error al eliminar la etnia'             
+        },
+        instruccion: {
+            createS: 'El nivel academico ha sido registrado',
+            createF: 'El nivel academico no pudo ser creado',
+            deleteS: 'El nivel academico ha sido eliminado',
+            deleteF: 'Error al eliminar el nivel academico'             
+        },
+        seguro: {
+            createS: 'El seguro ha sido registrado',
+            createF: 'El seguro no pudo ser creado',
+            deleteS: 'El seguro ha sido eliminado',
+            deleteF: 'Error al eliminar el seguro'             
+        },
+        acuerdo: {
+            createS: 'El acuerdo ha sido registrado',
+            createF: 'El acuerdo no pudo ser creado',
+            deleteS: 'El acuerdo ha sido eliminado',
+            deleteF: 'Error al eliminar acuerdo'             
+        },
+        zona: {
+            createS: 'La zona ha sido registrada',
+            createF: 'La zona no pudo ser creada',
+            deleteS: 'La zona ha sido eliminada',
+            deleteF: 'Error al eliminar la zona'             
+        },
+        tipoafiliado: {
+            createS: 'El tipo de afiliado ha sido registrado',
+            createF: 'El tipo de afiliado no pudo ser creado',
+            deleteS: 'El tipo de afiliado ha sido eliminado',
+            deleteF: 'Error al eliminar el tipo de afiliado'             
+        },
+        paciente: {
+            createS: 'El paciente ha sido registrado y una nueva ficha medica ha sido registrada',
+            createF: 'El paciente no pudo ser registrado',
+            updateS: 'El paciente ha sido actualizado exitosamente',
+            updateF: 'El paciente no pudo ser actualizado',
+            deleteS: 'El paciente ha sido eliminado',
+            deleteF: 'El paciente no puede ser eliminado no se han cumplido los requisitos necesarios',
+            deleteF2: 'El paciente no puede ser eliminado existen dependencias por lo tanto hay cobros pendientes', 
+        },
+        fichamedica: {
+            createS: 'El reingreso se ha generado',
+            createF: 'La informacion enviada no es valida',
+            createF2: 'La ficha medica no se ha creado',
+            updateS: 'El reingreso se ha generado',
+            updateF: 'Error al actualizar la ficha medica',
+            deleteS: 'El paciente se le ha dado de alta',
+            deleteF: 'No se ha podido dar de alta al paciente'             
+        },
+//---------------------------------------------------- Mensajes de Paciente Fin ----------------------------------------------------
+
+
+//--------------------------------------------------- Mensajes de Nutricion Inicio -------------------------------------------------
+        habitacion: {
+            createS: 'La habitación ha sido creada',
+            createF: 'La habitación no pudo ser creada',
+            deleteS: 'La habitación se ha eliminado',
+            deleteF: 'Error al eliminar la habitación'             
+        },
+        cama: {
+            createS: 'La cama ha sido creada',
+            createF: 'La cama no pudo ser creada',
+            updateS: 'La cama ha sido actualizada',
+            updateF: 'La cama no pudo ser actulizada',
+            deleteS: 'La cama se ha eliminado',
+            deleteF: 'Error al eliminar la cama',
+            trasladoS: 'El paciente ha sido trasladado',
+            trasladoF: 'La cama esta ocupada por otro paciente',
+            trasladoF2: 'Error al trasladar al paciente'         
+        },
+        tipo: {
+            createS: 'El tipo ha sido creado',
+            createF: 'El tipo no pudo ser creado',
+            deleteS: 'El tipo se ha eliminado',
+            deleteF: 'Error al eliminar el tipo'             
+        },
+        tipodieta: {
+            createS: 'El tipo de dieta ha sido creado',
+            createF: 'El tipo de dieta no pudo ser creado',
+            deleteS: 'El tipo de dieta se ha eliminado',
+            deleteF: 'Error al eliminar el tipo de dieta'             
+        },
+        nutricion: {
+            createS: 'El plan nutricional se añadio',
+            createF: 'El plan nutricional no pudo ser agregado',
+            deleteS: 'El plan nutricional se ha actualizado',
+            deleteF: 'Error al actualizar el plan nutricional'             
+        },
+//---------------------------------------------------- Mensajes de Nutricion Fin ---------------------------------------------------------
+
+
+//---------------------------------------------------- Mensajes de  Inicio ---------------------------------
+        s:{
             createS: '',
             createF: '',
             deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
-        s: {
-            createS: '',
-            createF: '',
-            deleteS: '',
-            deleteF: ''             
-        },
+            deleteF: ''
+        }
+
     }
 };
 
--- src/router/nutricion.js
+++ src/router/nutricion.js
@@ -2,17 +2,40 @@ const { Router }=require('express');
 const router=Router();
 
 //HABITACION
+const {  } =require('../controllers/nutricion.controller');
 //CAMA
+const {  } =require('../controllers/nutricion.controller');
 //TIPO
+const {  } =require('../controllers/nutricion.controller');
 //TIPODIETA
+const {  } =require('../controllers/nutricion.controller');
 //NUTRICION
-const { getEtnia, createEtnia, deleteEtnia } =require('../controllers/nutricion.controller');
+const {  } =require('../controllers/nutricion.controller');
 
 //HABITACION
+router.get('',);
+router.get('',);
+router.post('',);
+router.delete('',);
 //CAMA
+router.get('',);
+router.get('',);
+router.post('',);
+router.delete('',);
 //TIPO
+router.get('',);
+router.get('',);
+router.post('',);
+router.delete('',);
 //TIPODIETA
+router.get('',);
+router.get('',);
+router.post('',);
+router.delete('',);
 //NUTRICION
-router.get('',getEtnia);
-router.post('',createEtniaAdmin);
-router.delete('',deleteEtniaAdmin);
+router.get('',);
+router.get('',);
+router.post('',);
+router.delete('',);
+
+module.exports = router;
